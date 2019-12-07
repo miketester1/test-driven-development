51ceb3bb2ef3d6fcba81b6658c98838a058e8411
miketester1 <miketester1@users.noreply.github.com>
2019-12-07 14:31:07 +0100

    kalkulator finkcje

--- kalkulator.py
+++ kalkulator.py
@@ -1,3 +1,19 @@
 def dodawanie():
     a, b = 2, 5
     return a+b
+
+
+
+
+
+# kalkulator
+def upper(s):
+
+    txt = s
+    x = txt.upper()
+    return x
+
+def title(s):
+    txt = s
+    x= txt.title()
+    return x
